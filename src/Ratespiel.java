c05ba21f3735d25f6495bff867ab1f23b11b719a
ℓιѕα. <lisa.beringer@stud.hs-mannheim.de>
2023-10-11 21:22:09 +0200

    Test

---
+++ src/Ratespiel.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Ratespiel {
+
+	public static void main(String[] args) {
+		// Scanner einbinden
+		Scanner sc = new Scanner(System.in);
+
+		// Zufallszahl zwischen 1 und 5 erzeugen
+		int rand = (int) (5 * Math.random() + 1);
+
+		System.out.println("Rate eine Zahl zwischen 1 und 5");
+
+		// number eingeben
+		int number = sc.nextInt();
+
+		// Wenn die Zufallszahl geraten dann "Super..." ausgeben
+		if (rand == number) {
+			System.out.println("Super, richtig geraten!");
+		}
+		System.out.println("Sie haben die Zahl " + number + " geraten. Es war die Zahl " + rand);
+	}
+
+}
---
+++ src/TestClass.java
@@ -0,0 +1,15 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 3;
+		int z = x + y;
+		
+		System.out.println(z);
+		System.out.println("dog");
+
+	}
+
+}
